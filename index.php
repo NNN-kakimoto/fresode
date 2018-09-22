d486dcd9e36b83d05e65f04c9ba241205303d76c
Kai4510471 <aria7716@gmail.com>
2018-09-22 23:30:27 +0900

    API周り完成

--- index.php
+++ index.php
@@ -13,12 +13,51 @@ include 'src/key.php';
 <body>
 
 <script type="text/javascript">
-    function processImage() {
+    $(function(){
+        $(document).on('change','#image-select',function(){
+            console.log('aaa');
+            var blob = document.getElementById('image-select').files[0];
+            var formData = new FormData();
+            formData.append('image', blob);
+            // this.uploadImage(formData);
+            // this.writed = true;	
+            // var formData = new FormData();
+            // if ($("input[name='image_area']").val()!== '') {
+            // 	formData.append( "file", $("input[name='image_area']").prop("files")[0] );
+            // }
+            // formData.append('image', blob);
+            var addres = $.ajax({
+                contentType: false,
+                data: formData,
+                datatype: 'json',	
+                headers: {
+                    Authorization: 'Client-ID ee3b05e88daaede',
+                },
+                processData: false,
+                type: 'POST',
+                url: 'https://api.imgur.com/3/image'
+            }).done(function(data){
+                console.log(data);
+                var addres = data.data.link;
+                console.log(addres);
+                
+                processImage(addres);
+            }).fail(function(jqXHR, textStatus, errorThrown) {
+                var errorString = (errorThrown === '') ? 'Error. ' : errorThrown + ' (' + jqXHR.status + '): ';
+                errorString += (jqXHR.responseText === '') ? '' : (jQuery.parseJSON(jqXHR.responseText).message) ?
+                jQuery.parseJSON(jqXHR.responseText).message : jQuery.parseJSON(jqXHR.responseText).error.message;
+		    });
+        });
+
+           function processImage(url) {
         // **********************************************
         // *** Update or verify the following values. ***
         // **********************************************
 
         // Replace <Subscription Key> with your valid subscription key.
+        
+    //API切り替え
+    
         var subscriptionKey = '<?php echo $key1; ?>';
 
         // You must use the same region in your REST call as you used to get your
@@ -39,8 +78,7 @@ include 'src/key.php';
         };
 
         // Display the image.
-        var sourceImageUrl = document.getElementById("inputImage").value;
-        document.querySelector("#sourceImage").src = sourceImageUrl;
+        document.querySelector("#sourceImage").src = url;
 
         // Make the REST API call.
         $.ajax({
@@ -56,12 +94,23 @@ include 'src/key.php';
             type: "POST",
 
             // Request body.
-            data: '{"url": ' + '"' + sourceImageUrl + '"}',
+            data: '{"url": ' + '"' + url + '"}',
         })
 
         .done(function(data) {
             // Show formatted JSON on webpage.
-            $("#responseTextArea").val(JSON.stringify(data, null, 2));
+             //$("#responseTextArea").val(
+            // console.log(JSON.stringify(data, null, 2))//)
+            console.log(data);
+            console.log(data.description.tags);
+            arraytags = data.description.tags;
+            $.each(arraytags, function(index, value) {
+                if (value == 'メガネ'){
+                    console.log('フリソ！');
+                }
+                
+             })
+            
         })
 
         .fail(function(jqXHR, textStatus, errorThrown) {
@@ -72,16 +121,21 @@ include 'src/key.php';
                 jQuery.parseJSON(jqXHR.responseText).message;
             alert(errorString);
         });
+        
     };
+
+    });
+ 
 </script>
 
+<input type="file" id="image-select" >
 <h1>Analyze image:</h1>
 Enter the URL to an image, then click the <strong>Analyze image</strong> button.
 <br><br>
 Image to analyze:
 <input type="text" name="inputImage" id="inputImage"
     value="http://upload.wikimedia.org/wikipedia/commons/3/3c/Shaki_waterfall.jpg" />
-<button onclick="processImage()">Analyze image</button>
+<button onclick="processImage('https://i.imgur.com/mv54xNm.jpg')">Analyze image</button>
 <br><br>
 <div id="wrapper" style="width:1020px; display:table;">
     <div id="jsonOutput" style="width:600px; display:table-cell;">
